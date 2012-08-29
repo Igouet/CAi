f08f369bafe0512f98db8862625982987523768b
Roberto Martínez <robertomstocker@gmail.com>
2012-08-28 22:37:39 -0400

    Nuevo diseño y arreglo creación de cuenta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 # encoding: UTF-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper :all
   include SessionsHelper
   include ApplicationHelper
 
   before_filter :buscar_posts_populares
   before_filter :buscar_proximos_eventos
+
+  private
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+  helper_method :mobile_device?
+
 end
