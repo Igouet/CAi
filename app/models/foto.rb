51f7867e4419328635ae5cfa4cf93ac0494b11e4
Roberto Martínez <robertomstocker@gmail.com>
2012-09-03 23:30:43 -0300

    Agregados colores para eventos.
    Formato de fechas mejorado, agregado tamaño de fotos en modelo para
    mostrar álbum de fotos completo.

--- app/models/foto.rb
+++ app/models/foto.rb
@@ -7,11 +7,12 @@ class Foto < ActiveRecord::Base
   has_attached_file :avatar,
                     :default_url => '/assets/avatars/default_foto_picture_:style.png',
                     :url => '/system/:class/:attachment/:id/:style/:filename',
-                    :styles => { :grande => "935x700#",
-                                 :izquierda => "850x570#",
-                                 :viewport => "483x181#",
-                                 :rectangulo => "440x161#",
-                                 :miniatura => "144x161#"
+                    :styles => {:grande => "935x700#",
+                                :izquierda => "850x570#",
+                                :viewport => "483x181#",
+                                :rectangulo => "440x161#",
+                                :thumb => "180x120#",
+                                :miniatura => "144x161#"
                     }
 
 end
